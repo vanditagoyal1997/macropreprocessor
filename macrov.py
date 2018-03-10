7f6a6de346024a025cf5e901e0ee18164d5c0075
Vandita Goyal <vanditagoyal1997@gmail.com>
2018-03-10 17:44:32 +0530

    Added comments

--- macrov.py
+++ macrov.py
@@ -19,34 +19,34 @@ for i in range(len(s)):
 		while (s[i]!="$endmacro"):
 			lprog.append(s[i])
 			i+=1
-		macro[l[1]]=lprog
-		if len(l)>2:
-			if "," in l[2]:
+		macro[l[1]]=lprog #creating a dictionary for storing macro name with the actual definition
+		if len(l)>2: #if there are parameters
+			if "," in l[2]: #if there are more than one parameters
 				l[2]=l[2].split(",")
 				for j in range(len(l[2])):
 					l[2][j]=l[2][j].strip("$")
-					if "=" in l[2][j]:
+					if "=" in l[2][j]: #if there is any default value for parameters
 						l[2][j]=l[2][j].split("=")
 						lpar.append(l[2][j][0])
 						ldef.append(l[2][j][1])
 					else:
 						lpar.append(l[2][j])
-			else:
+			else: #if there is only one parameter
 				l[2]=l[2].strip("$")
 				lpar.append(l[2])
 		macropar[l[1]]=lpar
 		macrodef[l[1]]=ldef
 		macroarg[l[1]]=[]
 			
-print (macro)
-print (macropar)
+#print (macro)
+#print (macropar)
 
 for w in range(len(s)):
 	l=s[w].split(" ")
-	if l[0] in macro.keys():
-		if len(l)>1:
+	if l[0] in macro.keys(): #detecting macro calls
+		if len(l)>1:#if arguments are given
 			a=l[1]
-			if "," in a:
+			if "," in a:#if more than one arguments are given
 				a=a.split(",")
 			'''if len(a)>len(macropar[l[0]]):
 				print("error1")
@@ -54,7 +54,7 @@ for w in range(len(s)):
 			larg=[]
 			if len(a)==len(macropar[l[0]]):
 				for j in range(len(a)):
-					if "=" in a[j]:
+					if "=" in a[j]: #for keyword arguments
 						v=a[j].split("=")
 						v[0]=v[0].strip("$")
 						larg.append(v[1])
@@ -62,16 +62,16 @@ for w in range(len(s)):
 						larg.append(a[j])
 			
 				
-			print (str(w) +":")
+			#print (str(w) +":")
 			#print (larg)
 			macroarg[l[0]]=larg
-			print (macroarg[l[0]])
-			for i in range (len(macro[l[0]])):
+			#print (macroarg[l[0]])
+			for i in range (len(macro[l[0]])): #subsituting arguments
 				d=macro[l[0]][i]
 				if "$" in d:
 					d=d.split("$")	
 					res=d[0]
-					if len(d)>2:
+					if len(d)>2:#for more than one parameter subsitution in a line
 						for j in range(len(d)):
 								g=0
 								word=''
@@ -95,7 +95,7 @@ for w in range(len(s)):
 								res=res+d[j]
 						
 							
-					else:
+					else: #for only one parameter subsitution in one line
 						for k in range(len(macropar[l[0]])):
 							if macropar[l[0]][k]==d[1]:
 								if macroarg[l[0]][k]:
@@ -107,7 +107,7 @@ for w in range(len(s)):
 				else:
 					res=d
 				f2.write(res+"\n")
-		else:
+		else: #for no parameter subsitution in an expansion
 			for i in range(len(macro[l[0]])):
 				f2.write(macro[l[0]][i]+"\n")
 			
